c1a396bdabe7aa469f4c67ef69cb9fa61e807d1b
Andrew Cunningham <andrew@Andrews-MacBook-Pro.local>
2019-09-05 15:09:07 -0700

    trying new db url

--- src/ducks/dbURL.js
+++ src/ducks/dbURL.js
@@ -1,5 +1,5 @@
 // export const baseUrl = "https://postgres://rswbqlpbjklktf:e0240147979ff029f6414fa0ac52a36d37e1869b1fdddedbf8d53a679b4f66ff@ec2-107-22-160-185.compute-1.amazonaws.com:5432/d5t813qaopr4br"
-export const baseUrl ="https://productivity-server-ac.herokuapp.com/"
+export const baseUrl ="https://productivity-server-ac.herokuapp.com"
 //   process.env.NODE_ENV === "development"
 //     ? "http://localhost:8080"
 //     : "postgres://rswbqlpbjklktf:e0240147979ff029f6414fa0ac52a36d37e1869b1fdddedbf8d53a679b4f66ff@ec2-107-22-160-185.compute-1.amazonaws.com:5432/d5t813qaopr4br";
