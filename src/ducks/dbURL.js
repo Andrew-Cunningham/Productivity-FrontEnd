35e847ff8ea7a2b65744ac6aa8ad6dd8add6246a
Andrew Cunningham <andrew@Andrews-MacBook-Pro.local>
2019-09-05 14:41:07 -0700

    trying new db url

--- src/ducks/dbURL.js
+++ src/ducks/dbURL.js
@@ -1,4 +1,5 @@
-export const baseUrl = "https://postgres://rswbqlpbjklktf:e0240147979ff029f6414fa0ac52a36d37e1869b1fdddedbf8d53a679b4f66ff@ec2-107-22-160-185.compute-1.amazonaws.com:5432/d5t813qaopr4br"
+// export const baseUrl = "https://postgres://rswbqlpbjklktf:e0240147979ff029f6414fa0ac52a36d37e1869b1fdddedbf8d53a679b4f66ff@ec2-107-22-160-185.compute-1.amazonaws.com:5432/d5t813qaopr4br"
+export const baseUrl ="postgres://tywfywjefsfrmu:89d94c992870aad7d22a0437ecdb2936317c203cfc21252f8a1b9e63a2c5f169@ec2-184-73-232-93.compute-1.amazonaws.com:5432/dekah8j81er7mq"
 //   process.env.NODE_ENV === "development"
 //     ? "http://localhost:8080"
 //     : "postgres://rswbqlpbjklktf:e0240147979ff029f6414fa0ac52a36d37e1869b1fdddedbf8d53a679b4f66ff@ec2-107-22-160-185.compute-1.amazonaws.com:5432/d5t813qaopr4br";
